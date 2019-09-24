0f4e2502953f6ab32a1b1affa8268a95f3a25698
AlvaroSebastian98 <alvaro.sebastian98@gmail.com>
2019-09-23 23:24:00 -0500

    first commit

---
+++ Ejercicio04/ITextToSpeech.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace Ejercicio04
+{
+    public interface ITextToSpeech
+    {
+        void Speak(string text);
+    }
+}
---
+++ Ejercicio04/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio04
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new BatteryDemo());
+
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new TextToSpeechDemo());
+            };
+
+        }
+    }
+}
